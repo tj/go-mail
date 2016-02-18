a63bae7f140555f407ca06a2a70777df36ef92e0
TJ Holowaychuk <tj@vision-media.ca>
2016-02-17 16:36:45 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# go-ses
+
+Package ses provides a small wrapper around AWS SES.
+
+## Badges
+
+[![GoDoc](https://godoc.org/github.com/tj/go-ses?status.svg)](https://godoc.org/github.com/tj/go-ses)
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+![](https://img.shields.io/badge/status-stable-green.svg)
+
+---
+
+> [tjholowaychuk.com](http://tjholowaychuk.com) &nbsp;&middot;&nbsp;
+> GitHub [@tj](https://github.com/tj) &nbsp;&middot;&nbsp;
+> Twitter [@tjholowaychuk](https://twitter.com/tjholowaychuk)
---
+++ ses.go
@@ -0,0 +1,58 @@
+// Package ses provides a small wrapper around AWS SES.
+package ses
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/aws/aws-sdk-go/service/ses/sesiface"
+)
+
+// SES alert support.
+type SES struct {
+	Service sesiface.SESAPI // Service implementation
+}
+
+// Email options.
+type Email struct {
+	From    string   // From source email
+	To      []string // To destination email(s)
+	Subject string   // Subject text to send
+	Text    string   // Text is the text body representation
+	HTML    string   // HTMLBody is the HTML body representation
+}
+
+// SendEmail message.
+func (s *SES) SendEmail(email Email) error {
+	if email.HTML == "" {
+		email.HTML = email.Text
+	}
+
+	msg := &ses.Message{
+		Subject: &ses.Content{
+			Charset: aws.String("utf-8"),
+			Data:    &email.Subject,
+		},
+		Body: &ses.Body{
+			Html: &ses.Content{
+				Charset: aws.String("utf-8"),
+				Data:    &email.HTML,
+			},
+			Text: &ses.Content{
+				Charset: aws.String("utf-8"),
+				Data:    &email.Text,
+			},
+		},
+	}
+
+	dest := &ses.Destination{
+		ToAddresses: aws.StringSlice(email.To),
+	}
+
+	_, err := s.Service.SendEmail(&ses.SendEmailInput{
+		Source:      &email.From,
+		Destination: dest,
+		Message:     msg,
+	})
+
+	return err
+}
